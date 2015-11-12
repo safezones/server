2d8f2c2b26957e9fc2fc6537ce6cec32be3eee6b
Your Name <dkgs1998@gmail.com>
2015-11-12 17:28:16 +0200

    fixed stuff

--- db/migrate/20151112151351_change_category_to_zone_category_in_safe_zones.rb
+++ db/migrate/20151112151351_change_category_to_zone_category_in_safe_zones.rb
@@ -1,5 +1,5 @@
 class ChangeCategoryToZoneCategoryInSafeZones < ActiveRecord::Migration
   def change
-  		change_column :safe_zones, :category_id, :zone_category_id, :default => 0.0
+  	rename_column :safe_zones, :category_id, :zone_category_id
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -34,8 +34,15 @@ ActiveRecord::Schema.define(version: 20151112151351) do
     t.datetime "updated_at",               null: false
   end
 
-# Could not dump table "safe_zones" because of following NoMethodError
-#   undefined method `[]' for nil:NilClass
+  create_table "safe_zones", force: :cascade do |t|
+    t.float    "lat"
+    t.float    "lon"
+    t.integer  "zone_category_id"
+    t.float    "radius"
+    t.string   "address"
+    t.datetime "created_at",       null: false
+    t.datetime "updated_at",       null: false
+  end
 
   create_table "zone_categories", force: :cascade do |t|
     t.datetime "created_at", null: false
